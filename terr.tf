4854884deba78cf05c80a6eb00b4490b5421c4fb
RJbagawathy <86833328+RJbagawathy@users.noreply.github.com>
2021-12-29 19:01:27 +0530

    Update terr.tf

--- terr.tf
+++ terr.tf
@@ -1,8 +1,15 @@
-terraform {
-   backend "azurerm" {
+  terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
   }
-}
+  
 
+ backend "azurerm" {
+  }
+     
 provider "azurerm" {
   features {}  
 
