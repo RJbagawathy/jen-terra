cdcc57745a18efa241a8438c1d872e807db0233b
RJbagawathy <86833328+RJbagawathy@users.noreply.github.com>
2021-12-29 19:07:33 +0530

    Update terr.tf

--- terr.tf
+++ terr.tf
@@ -2,7 +2,6 @@
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.46.0"
     }
   }
   }
@@ -12,14 +11,7 @@
      
 provider "azurerm" {
   features {}  
-
- 
-
 }
-
-
-
-
 
 
 resource "azurerm_resource_group" "rg" {
