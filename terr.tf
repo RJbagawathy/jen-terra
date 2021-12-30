927895be1f4fbfe79de2f55f9892420fde0a8220
RJbagawathy <86833328+RJbagawathy@users.noreply.github.com>
2021-12-30 15:33:16 +0530

    Update terr.tf

--- terr.tf
+++ terr.tf
@@ -1,11 +1,3 @@
-
-variable "csec" {
-}
-
-
-
-
-
 terraform {
   backend "azurerm" {
         resource_group_name  = "jenkins-get-started-rg1"
@@ -20,10 +12,6 @@ terraform {
 provider "azurerm" {
   features {} 
   
-  subscription_id = "11053afc-3c4e-4207-a817-c33c75b65866"
-  client_id       ="8ae49cf9-7c95-4e0b-a7c3-e8056415aa7e"
-  client_secret   = var.csec
-  tenant_id       = "d518bac2-5294-4997-aa33-d4f6fd3a4844"
 }
 
 
